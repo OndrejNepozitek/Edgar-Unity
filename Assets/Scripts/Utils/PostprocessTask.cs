5fce791d6c8e6f1fbb880c6bdced201e797ad1a1
OndrejNepozitek <ondra@nepozitek.cz>
2018-12-16 22:15:57 +0100

    Add a pipeline task that centers the layout after it is generated

---
+++ Assets/Scripts/Utils/PostprocessTask.cs
@@ -0,0 +1,28 @@
+﻿namespace Assets.Scripts.Utils
+{
+	using GeneratorPipeline;
+	using Pipeline;
+	using UnityEngine;
+
+	[CreateAssetMenu(menuName = "Pipeline tasks/Postprocess")]
+	public class PostprocessTask : PipelineTask
+	{
+		public bool CenterGrid = true;
+	}
+
+	[PipelineTaskFor(typeof(PostprocessTask))]
+	public class PostprocessTask<T> : IConfigurablePipelineTask<T, PostprocessTask>
+		where T : IGeneratorPayload
+	{
+		public PostprocessTask Config { get; set; }
+
+		public void Process(T payload)
+		{
+			if (Config.CenterGrid)
+			{
+				payload.Tilemaps[0].ResizeBounds();
+				payload.Tilemaps[0].transform.parent.position = -payload.Tilemaps[0].cellBounds.center;
+			}
+		}
+	}
+}
