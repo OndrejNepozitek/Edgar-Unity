af3bcc18e1f83905525b6ba0bead93feb4e42230
Ondřej Nepožitek <ondra@nepozitek.cz>
2020-05-30 17:02:20 +0200

    Example of how the detect the current active room

---
+++ Examples/CurrentRoomDetection/Scripts/CurrentRoomDetectionPostProcess.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using ProceduralLevelGenerator.Unity.Generators.Common;
+using ProceduralLevelGenerator.Unity.Generators.Common.RoomTemplates;
+using ProceduralLevelGenerator.Unity.Generators.DungeonGenerator.PipelineTasks;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace ProceduralLevelGenerator.Unity.Examples.CurrentRoomDetection.Scripts
+{
+    [CreateAssetMenu(menuName = "Dungeon generator/Examples/Current room detection/Post process", fileName = "CurrentRoomDetectionPostProcess")]
+    public class CurrentRoomDetectionPostProcess : DungeonGeneratorPostProcessBase
+    {
+        public override void Run(GeneratedLevel level, LevelDescription levelDescription)
+        {
+            foreach (var roomInstance in level.GetRoomInstances())
+            {
+                var roomTemplateInstance = roomInstance.RoomTemplateInstance;
+
+                // Find floor tilemap layer
+                var tilemaps = RoomTemplateUtils.GetTilemaps(roomTemplateInstance);
+                var floor = tilemaps.Single(x => x.name == "Floor").gameObject;
+
+                // Add floor collider
+                AddFloorCollider(floor);
+
+                // Add the room manager component
+                var roomManager = roomTemplateInstance.AddComponent<CurrentRoomDetectionRoomManager>();
+                roomManager.RoomInstance = roomInstance;
+
+                // Add current room detection handler
+                floor.AddComponent<CurrentRoomDetectionTriggerHandler>();
+            }
+        }
+
+        protected void AddFloorCollider(GameObject floor)
+        {
+            var tilemapCollider2D = floor.AddComponent<TilemapCollider2D>();
+            tilemapCollider2D.usedByComposite = true;
+
+            var compositeCollider2d = floor.AddComponent<CompositeCollider2D>();
+            compositeCollider2d.geometryType = CompositeCollider2D.GeometryType.Polygons;
+            compositeCollider2d.isTrigger = true;
+            compositeCollider2d.generationType = CompositeCollider2D.GenerationType.Manual;
+
+            floor.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        }
+    }
+}
---
+++ Examples/CurrentRoomDetection/Scripts/CurrentRoomDetectionRoomManager.cs
@@ -0,0 +1,33 @@
+﻿using ProceduralLevelGenerator.Unity.Generators.Common.Rooms;
+using UnityEngine;
+
+namespace ProceduralLevelGenerator.Unity.Examples.CurrentRoomDetection.Scripts
+{
+    public class CurrentRoomDetectionRoomManager : MonoBehaviour
+    {
+        /// <summary>
+        /// Room instance of the corresponding room.
+        /// </summary>
+        public RoomInstance RoomInstance;
+
+        /// <summary>
+        /// Gets called when a player enters the room.
+        /// </summary>
+        /// <param name="player"></param>
+        public void OnRoomEnter(GameObject player)
+        {
+            Debug.Log($"Room enter. Room name: {RoomInstance.Room.Name}, Room template: {RoomInstance.RoomTemplatePrefab.name}");
+            CurrentRoomDetectionGameManager.Instance.OnRoomEnter(RoomInstance);
+        }
+
+        /// <summary>
+        /// Gets called when a player leaves the room.
+        /// </summary>
+        /// <param name="player"></param>
+        public void OnRoomLeave(GameObject player)
+        {
+            Debug.Log($"Room leave {RoomInstance.Room.Name}");
+            CurrentRoomDetectionGameManager.Instance.OnRoomLeave(RoomInstance);
+        }
+    }
+}
