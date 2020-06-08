d020b8e8363f434c06def1855dc286390cdb5346
Ondřej Nepožitek <ondra@nepozitek.cz>
2020-06-08 22:08:07 +0200

    Improve docs

--- Runtime/Generators/Common/Utils/GeneratorConstants.cs
+++ Runtime/Generators/Common/Utils/GeneratorConstants.cs
@@ -2,10 +2,19 @@
 {
     public static class GeneratorConstants
     {
+        /// <summary>
+        /// Name of the game object that holds tilemaps layers. This name is used both in room templates and in generated levels.
+        /// </summary>
         public static string TilemapsRootName = "Tilemaps";
 
+        /// <summary>
+        /// Name of the game object that holds instance of all the room templates that are used in a generated level.
+        /// </summary>
         public static string RoomsRootName = "Rooms";
 
-        public static string OutlineOverrideName = "Outline Override";
+        /// <summary>
+        /// Name of the Outline Override tilemap layer.
+        /// </summary>
+        public static string OutlineOverrideLayerName = "Outline Override";
     }
 }
