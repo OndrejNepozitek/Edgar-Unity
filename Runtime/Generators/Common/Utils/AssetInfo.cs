a9cbaaf7f52f65397670a7a4f97266c9caa8b2ba
Ondřej Nepožitek <ondra@nepozitek.cz>
2021-06-03 18:27:00 +0200

    Update versions

--- Runtime/Generators/Common/Utils/AssetInfo.cs
+++ Runtime/Generators/Common/Utils/AssetInfo.cs
@@ -2,6 +2,6 @@
 {
     public static class AssetInfo
     {
-        public static string Version = "2.0.0-alpha.8-hotfix.0";
+        public static string Version = "2.0.0-alpha.8-hotfix.1";
     }
 }
