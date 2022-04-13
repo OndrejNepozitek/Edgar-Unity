6532599d1a1f5dfb26f762a49d073eef340877c9
OndrejNepozitek <ondra@nepozitek.cz>
2022-04-13 10:57:35 +0200

    Edgar project settings POC

---
+++ Editor/Settings/EdgarSettings.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using UnityEditorInternal;
+using UnityEngine;
+
+namespace Edgar.Unity.Editor
+{
+    #if UNITY_2020_1_OR_NEWER
+    public class EdgarSettings : ScriptableSingleton<EdgarSettings>
+    #else
+    public class EdgarSettings : EdgarScriptableSingleton<EdgarSettings>
+        #endif
+    {
+        internal const string FilePath = "EdgarSettings.asset";
+        internal const string FullPath = "ProjectSettings/" + FilePath;
+
+        private void OnEnable()
+        {
+            hideFlags &= ~HideFlags.NotEditable;
+        }
+
+        public void Save()
+        {
+            #if UNITY_2020_1_OR_NEWER
+            Save(True);
+            #else
+            var path = FullPath;
+            var directoryName = Path.GetDirectoryName(path);
+            if (!Directory.Exists(directoryName))
+            {
+                if (directoryName != null) Directory.CreateDirectory(directoryName);
+            }
+
+            InternalEditorUtility.SaveToSerializedFileAndForget(new Object[]
+            {
+                instance
+            }, path, true);
+            #endif
+        }
+    }
+}
---
+++ Editor/Settings/EdgarSettingsProvider.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Edgar.Unity.Editor
+{
+    public class EdgarSettingsProvider : SettingsProvider
+    {
+        SerializedObject serializedObject;
+
+        public EdgarSettingsProvider(string path, SettingsScope scopes, IEnumerable<string> keywords = null)
+            : base(path, scopes, keywords) { }
+
+        public override void OnActivate(string searchContext, VisualElement rootElement)
+        {
+            EdgarSettings.instance.Save();
+            serializedObject = new SerializedObject(EdgarSettings.instance);
+        }
+
+        public override void OnGUI(string searchContext)
+        {
+            using (CreateSettingsWindowGUIScope())
+            {
+                serializedObject.Update();
+                EditorGUI.BeginChangeCheck();
+
+                //EditorGUILayout.PropertyField(serializedObject.FindProperty(nameof(EdgarSettings.Test3)));
+
+                if (EditorGUI.EndChangeCheck())
+                {
+                    serializedObject.ApplyModifiedProperties();
+                    EdgarSettings.instance.Save();
+                }
+            }
+        }
+
+        [SettingsProvider]
+        public static SettingsProvider CreateMySingletonProvider()
+        {
+            var provider = new EdgarSettingsProvider("Project/Edgar", SettingsScope.Project);
+            return provider;
+        }
+
+        private IDisposable CreateSettingsWindowGUIScope()
+        {
+            var unityEditorAssembly = Assembly.GetAssembly(typeof(EditorWindow));
+            var type = unityEditorAssembly.GetType("UnityEditor.SettingsWindow+GUIScope");
+            return Activator.CreateInstance(type) as IDisposable;
+        }
+    }
+}
